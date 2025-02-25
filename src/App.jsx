44a634486de542650430fe78a9d41612ac018def
moldenburger <maritoldenburger@hotmail.com>
2025-02-25 18:07:37 +0100

    Opdracht 2 af

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import './App.css';
 import axios from 'axios';
 import React, {useEffect, useState} from "react";
 import worldMap from "./assets/world_map.png";
+import globe from "./assets/globe.gif";
 import getRegionColour from "./helpers/getRegionColour.jsx";
+import numberToMillion from "./helpers/numberToMillion.jsx";
 
 function App() {
 
     const [countries, setCountries] = useState([]);
+    const [countryInfo, setCountryInfo] = useState({});
+    const [searchQuery, setSearchQuery] = useState("");
+    const [error, setError] = useState("");
 
     async function fetchCountries() {
 
@@ -20,35 +25,85 @@ function App() {
         }
     }
 
+    async function fetchCountryInfo(e) {
+        e.preventDefault();
+        setError("");
+
+        try {
+            const result = await axios.get(`https://restcountries.com/v3.1/name/${searchQuery}`);
+            setCountryInfo(result.data[0])
+            console.log(result.data[0]);
+            setSearchQuery("");
+
+        } catch (error) {
+            console.error(error);
+            setError(`${searchQuery} bestaat niet. Probeer het opnieuw.`)
+        }
+    }
+
     return (
         <>
             <header>
                 <img src={worldMap} alt="World map"/>
             </header>
             <main>
-                {countries.length > 0
-                    ? (
-                        <ul>
-                            {countries
-                                .sort((a, b) => a.population - b.population)
-                                .map((country) => (
-                                    <li key={country.cca3}>
-                                        <img
-                                            src={country.flags.png}
-                                            alt={`Flag of ${country.name.common}`}
-                                        />
-                                        <h4 className={getRegionColour(country.region)}>
-                                            {country.name.common}
-                                        </h4>
-                                        <p>Has a population of {country.population} people</p>
-                                    </li>
-                                ))}
-                        </ul>
-                    )
-                    : (
-                        <button className="button" onClick={fetchCountries}>Show all countries</button>
-                    )
-                }
+                <section>
+                    <h1>World Regions</h1>
+                    {countries.length > 0
+                        ? (
+                            <ul>
+                                {countries
+                                    .sort((a, b) => a.population - b.population)
+                                    .map((country) => (
+                                        <li key={country.cca3}>
+                                            <img
+                                                src={country.flags.png}
+                                                alt={`Vlag van ${country.name.common}`}
+                                            />
+                                            <h4 className={getRegionColour(country.region)}>
+                                                {country.name.common}
+                                            </h4>
+                                            <p>Has a population of {country.population} people</p>
+                                        </li>
+                                    ))}
+                            </ul>
+                        )
+                        : (
+                            <button className="button" onClick={fetchCountries}>Alle landen ophalen</button>
+                        )
+                    }
+                </section>
+                <section>
+                    <h1>Search country information</h1>
+                    <img src={globe} alt="Globe"/>
+                    <form className="search-bar" onSubmit={fetchCountryInfo}>
+                        <input
+                            type="search"
+                            name="search"
+                            placeholder="Bijvoorbeeld Nederland of Peru"
+                            value={searchQuery}
+                            onChange={(e) => setSearchQuery(e.target.value)}
+                        >
+                        </input>
+                        <button className="submit">Zoek</button>
+                        {error && <p>{error}</p>}
+                    </form>
+                    {countryInfo.name &&
+                        <>
+                            <img
+                                src={countryInfo.flags.png}
+                                alt={`Vlag van ${countryInfo.name.common}`}
+                            />
+                            <h1>{countryInfo.name.common}</h1>
+                            <p>{countryInfo.name.common} is situated in {countryInfo.subregion} and the capital
+                                is {countryInfo.capital}.</p>
+                            <p>It has a population of {numberToMillion(countryInfo.population)} people and it
+                                borders
+                                with {countryInfo.borders.length} neighboring countries </p>
+
+                        </>
+                    }
+                </section>
             </main>
             <footer>
             </footer>
---
+++ src/helpers/numberToMillion.jsx
@@ -0,0 +1,5 @@
+function numberToMillion(number) {
+    return Math.round(number / 1000000) + " million";
+}
+
+export default numberToMillion;
