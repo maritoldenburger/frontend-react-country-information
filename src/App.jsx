8296aa65ac9bfe0d690ec20f19b16b2fd9a90141
moldenburger <maritoldenburger@hotmail.com>
2025-02-26 15:49:13 +0100

    Styling toegevoegd

--- src/App.jsx
+++ src/App.jsx
@@ -44,32 +44,35 @@ function App() {
     return (
         <>
             <header>
-                <img src={worldMap} alt="World map"/>
+                <img src={worldMap} alt="World map" className="header-image"/>
             </header>
             <main>
                 <section>
                     <h1>World Regions</h1>
                     {countries.length > 0
                         ? (
-                            <ul>
+                            <ul className="countries-grid">
                                 {countries
                                     .sort((a, b) => a.population - b.population)
                                     .map((country) => (
                                         <li key={country.cca3}>
-                                            <img
-                                                src={country.flags.png}
-                                                alt={`Vlag van ${country.name.common}`}
-                                            />
-                                            <h4 className={getRegionColour(country.region)}>
-                                                {country.name.common}
-                                            </h4>
+                                            <div className="countries-title">
+                                                <img
+                                                    className="flag"
+                                                    src={country.flags.png}
+                                                    alt={`Vlag van ${country.name.common}`}
+                                                />
+                                                <h4 className={getRegionColour(country.region)}>
+                                                    {country.name.common}
+                                                </h4>
+                                            </div>
                                             <p>Has a population of {country.population} people</p>
                                         </li>
                                     ))}
                             </ul>
                         )
                         : (
-                            <button className="button" onClick={fetchCountries}>Alle landen ophalen</button>
+                            <button onClick={fetchCountries}>Alle landen ophalen</button>
                         )
                     }
                 </section>
@@ -85,23 +88,26 @@ function App() {
                             onChange={(e) => setSearchQuery(e.target.value)}
                         >
                         </input>
-                        <button className="submit">Zoek</button>
+                        <button>Zoek</button>
                         {error && <p>{error}</p>}
                     </form>
                     {countryInfo.name &&
-                        <>
-                            <img
-                                src={countryInfo.flags.png}
-                                alt={`Vlag van ${countryInfo.name.common}`}
-                            />
-                            <h1>{countryInfo.name.common}</h1>
+                        <div className="country-info-card">
+                            <div className="country-info-title">
+                                <img
+                                    className="flag"
+                                    src={countryInfo.flags.png}
+                                    alt={`Vlag van ${countryInfo.name.common}`}
+                                />
+                                <h2>{countryInfo.name.common}</h2>
+                            </div>
                             <p>{countryInfo.name.common} is situated in {countryInfo.subregion} and the capital
                                 is {countryInfo.capital}.</p>
                             <p>It has a population of {numberToMillion(countryInfo.population)} people and it
                                 borders
-                                with {countryInfo.borders.length} neighboring countries </p>
-
-                        </>
+                                with {countryInfo.borders.length} neighboring countries.</p>
+                            <p>Websites can be found on {countryInfo.tld} domains.</p>
+                        </div>
                     }
                 </section>
             </main>
